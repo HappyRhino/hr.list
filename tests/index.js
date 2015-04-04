e030136f236e3729286b8ce320ff75d801ef2325
Samy Pessé <samypesse@gmail.com>
2015-04-04 23:38:24 +0200

    Add base code and tests

--- tests/index.js
+++ tests/index.js
@@ -2,5 +2,49 @@ var List = require("../");
 
 describe('List', function() {
 
+    describe('count', function() {
+
+        it('should return correct count (without filter)', function() {
+            var l = new List();
+
+            l.collection.reset([
+                {
+                    'test': 1
+                },
+                {
+                    'test': 2
+                },
+                {
+                    'test': 3
+                }
+            ]);
+
+            expect(l.count()).to.equal(3);
+        });
+
+        it('should return correct count (with filter)', function() {
+            var l = new List({
+                filter: function(model) {
+                    return model.get("test") >= 2;
+                }
+            });
+
+            l.collection.reset([
+                {
+                    'test': 1
+                },
+                {
+                    'test': 2
+                },
+                {
+                    'test': 3
+                }
+            ]);
+
+            expect(l.count()).to.equal(2);
+        });
+
+    });
+
 });
 
