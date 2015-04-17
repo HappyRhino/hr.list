5c4315fe53e3787c50969c67197bb6448b344454
Samy Pesse <samypesse@gmail.com>
2015-04-17 16:42:24 -0500

    Add test for list display

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,4 @@
+var $ = require("jquery");
 var List = require("../");
 
 describe('List', function() {
@@ -42,6 +43,41 @@ describe('List', function() {
             ]);
 
             expect(l.count()).to.equal(2);
+        });
+
+        it('should correcly be rendered', function() {
+            var Item = List.Item.extend({
+                render: function() {
+                    this.html("test "+this.model.get('test'));
+                    return this.ready();
+                }
+            });
+
+            var TestList = List.extend({
+                Item: Item
+            });
+
+            var l = new TestList({
+
+            });
+
+            l.collection.reset([
+                {
+                    'test': 1
+                },
+                {
+                    'test': 2
+                },
+                {
+                    'test': 3
+                }
+            ]);
+
+            var $container = $("<div>", { 'class': "test"});
+            $container.appendTo($("body"));
+            l.appendTo($container);
+
+            expect($(".test").html()).to.equal("<ul><li>test 1</li><li>test 2</li><li>test 3</li></ul>");
         });
 
     });
